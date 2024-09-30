cbfb6684ad08b6ab89fc02e26ff77ba649689636
Jod Alef <jod.alef@gmail.com>
2024-09-30 05:40:21 -0300

    Melhoria no código do Simulador, adicionando tambem o lucro no periodo.

--- A2/exercicio4.py
+++ A2/exercicio4.py
@@ -4,15 +4,18 @@
 # simular o aumento do saldo com juros compostos ao longo dos anos e imprima o saldo final após o período especificado.
 
 capital = float(input("Digite o saldo inicial da conta:"))
-jurosAno = float(input("Digite a taxa de juros anual: "))/100
+jurosAno = float(input("Digite a porcentagem da taxa de juros anual: "))/100
 anos = int(input("Digite o numero de anos: "))
 anoAtual = 0
+capitalInicial = capital
 
 while anoAtual != anos:
     for n in range(anos):
         anoAtual += 1
         capital = capital * (1 + jurosAno)
-        print(f"Seu saldo após o {anoAtual}º ano é de {capital:.2f}")
+        print(f"Seu saldo após o {anoAtual}º ano é de R$ {capital:.2f}")
+    else:
+        print(f"Você lucrou R$ {(capital - capitalInicial):.2f} no periodo de {anos} anos")
 
 
 
