def04a1f594a5d0b06a74c7269f771b070fc0b3d
Jod Alef <jod.alef@gmail.com>
2024-10-12 06:11:25 -0300

    Correções de bugs referentes a inserções no banco de dados.

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
-import os.path
 from flask import Flask
 from configurations.database import db
 from controllers.task_controller import task_blueprint
-
+#from urllib.parse import quote
 
 app = Flask(__name__)
 app.register_blueprint(task_blueprint)
 
-base_dir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{os.path.join(base_dir, "task.db")}'
+user = 'postgres.xdaftsgbbbscfrueidzc'
+password = 'D6YJTdKyN9vZSfS1'
+# password = quote(password)
+host = 'aws-0-sa-east-1.pooler.supabase.com'
+db_name = 'postgres'
+port = 5432
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{user}:{password}@{host}:{port}/{db_name}'
 app.config['SQLALCHEMY_TRACK_NOTIFICATIONS'] = False
+#app.config´['SQLALCHEMY_ECHO'] = False
 
 db.init_app(app)
 
@@ -17,3 +23,5 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     app.run(debug=True)
+
+# user=postgres.xdaftsgbbbscfrueidzc password=[YOUR-PASSWORD] host=aws-0-sa-east-1.pooler.supabase.com port=6543 dbname=postgres
---
+++ repositories/task_repository.py
@@ -0,0 +1,38 @@
+from configurations.database import db
+from sqlalchemy import text
+
+from models.task_models import Task
+
+class TaskRepository:
+    @staticmethod
+    def get_all_tasks():
+        query = text('SELECT * FROM tasks')
+        result = db.session.execute(query)
+        return result.fetchall()
+
+    @staticmethod
+    def add_task(conteudo, prioridade="Média"):
+        query = text("""
+            INSERT INTO tasks (conteudo, completa, prioridade)
+            VALUES (:conteudo, :completa, :prioridade)
+        """)
+
+        db.session.execute(query, {
+            'conteudo' : conteudo,
+            'completa' : False,
+            'prioridade' : prioridade
+        })
+
+        db.session.commit()
+
+    @staticmethod
+    def complete_task(tarefa_id):
+        query = text("UPDATE tasks SET completa = TRUE WHERE id = :id")
+        db.session.execute(query,{'id', tarefa_id})
+        db.session.commit()
+
+    @staticmethod
+    def delete_task(tarefa_id):
+        query = text("DELETE FROM tasks WHERE id=:id")
+        db.session.execute(query, {'id', tarefa_id})
+        db.session.commit()
