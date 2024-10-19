03ba383a7f3bd19189e4efc2f037556cd77980c9
Jod Alef <jod.alef@gmail.com>
2024-10-18 21:15:40 -0300

    Correções

--- app.py
+++ app.py
@@ -1,19 +1,20 @@
 from flask import Flask
 
-from configurations.sql_commands import create_table_bikes
+from configurations.sql_commands import create_table_tasks, create_table_users, create_root_user
 from configurations.database import db
-from controllers.bike_controller import bike_blueprint
+from controllers.task_controller import task_blueprint
+from controllers.user_controller import user_blueprint
 from urllib.parse import quote
 from dotenv import load_dotenv
 import os
-import logging
 
 
 load_dotenv()
 
 app = Flask(__name__)
-app.register_blueprint(bike_blueprint)
 app.secret_key = os.getenv('SECRET_KEY')
+app.register_blueprint(task_blueprint)
+app.register_blueprint(user_blueprint)
 
 user = os.getenv('DB_USER')
 password = os.getenv('DB_PASSWORD')
@@ -24,14 +25,12 @@ passw = quote(password)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{user}:{passw}@{host}:{port}/{db_name}'
 app.config['SQLALCHEMY_TRACK_NOTIFICATIONS'] = False
-app.config['SQLALCHEMY_ECHO'] = False
-
-logging.basicConfig()
-logging.getLogger("sqlalchemy.engine").setLevel(logging.DEBUG)
+#app.config´['SQLALCHEMY_ECHO'] = False
 
 db.init_app(app)
 
-
 if __name__ == '__main__':
-    create_table_bikes(app,db)  # EXPLICAR ESSE APP, DB
+    create_table_tasks(app, db)
+    create_table_users(app, db)
+    create_root_user(app, db)
     app.run(debug=True)
---
+++ models/task_models.py
@@ -0,0 +1,20 @@
+from configurations.database import db
+
+
+class Task(db.Model):
+    __tablename__ = 'tasks'
+
+    id = db.Column(db.Integer, primary_key=True)
+    conteudo = db.Column(db.String(200), nullable=False)
+    completa = db.Column(db.Boolean, default=False)
+    prioridade = db.Column(db.String(50), nullable=False, default="Média")
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Tarefa('{self.conteudo}', '{self.completa}', '{self.prioridade}')"
+
+    def __str__(self):
+        return f"Conteúdo: {self.conteudo}, Completa: {self.completa}, Prioridade: {self.prioridade}"
+
+    def completar(self):
+        self.completa = True
